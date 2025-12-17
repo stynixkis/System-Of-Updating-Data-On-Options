ae1970ab71b61a209d40594f4da7c89013adae7f
stynixkis <orangegirlrussen@gmail.com>
2025-12-17 18:29:08 +0300

    Добавление логирования и обработки исключений

---
+++ SystemOfUpdatingDataOnOptions/Classes/ModelsSystem/Logging.cs
@@ -0,0 +1,36 @@
+﻿using log4net;
+using log4net.Appender;
+using log4net.Layout;
+using log4net.Repository.Hierarchy;
+
+namespace ParsingWebSite.Classes
+{
+    /// <summary>
+    /// Класс для создания логов
+    /// </summary>
+    public static class Logging
+    {
+        /// <summary>
+        /// Метод логирования
+        /// </summary>
+        public static void ConfigureFileLogging()
+        {
+            var hierarchy = (Hierarchy)LogManager.GetRepository();
+            var fileAppender = new FileAppender();
+            fileAppender.Name = "FileAppender";
+            fileAppender.File = $"../../../Logs/LogDataUpdate_{DateTime.Now.ToShortDateString()}.log";
+
+            fileAppender.AppendToFile = true;
+            var patternLayout = new PatternLayout();
+            patternLayout.ConversionPattern = "%date [%thread] %-5level - %message%newline";
+            patternLayout.ActivateOptions();
+
+            fileAppender.Layout = patternLayout;
+            fileAppender.ActivateOptions();
+
+            hierarchy.Root.AddAppender(fileAppender);
+            hierarchy.Root.Level = log4net.Core.Level.All;
+            hierarchy.Configured = true;
+        }
+    }
+}
