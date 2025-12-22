88eecb5b47aa5b8ffd404bf9c5487cf4a4b7828b
stynixkis <orangegirlrussen@gmail.com>
2025-12-22 21:43:34 +0300

    Обновление doxygen документации 2.0

--- html/namespace_parsing_web_site.js
+++
@@ -1,4 +0,0 @@
-var namespace_parsing_web_site =
-[
-    [ "Classes", "namespace_parsing_web_site_1_1_classes.html", null ]
-];
--- html/search/namespaces_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['testssystemofupdatingdataonoptions_0',['TestsSystemOfUpdatingDataOnOptions',['../namespace_tests_system_of_updating_data_on_options.html',1,'']]]
-];
